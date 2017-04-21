1747cf4ab44dc98066d3c0be150ce4845a76c1b9
StandAloneComplex666 <yc2739@nyu.edu>
2017-04-21 13:42:58 -0400

    Add files via upload

--- pytorch_nn.py
+++ pytorch_nn.py
@@ -53,15 +53,17 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv2 = nn.Conv2d(10, 500, kernel_size=5)
+	self.conv2_drop = nn.Dropout2d()
+	self.conv3 = nn.Conv2d(500, 2000, kernel_size=2)        
+        self.fc1 = nn.Linear(18000, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 18000)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
@@ -90,9 +92,9 @@ def train(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
-            print >> fresult, 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            fresult.write( 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                      epoch, batch_idx * len(data), len(train_loader.dataset),
-                     100. * batch_idx / len(train_loader), loss.data[0])
+                     100. * batch_idx / len(train_loader), loss.data[0]))
 
 def test(epoch):
     model.eval()
@@ -112,11 +114,12 @@ def test(epoch):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-    print >> fresult,'\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    fresult.write('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset))
+        100. * correct / len(test_loader.dataset)))
 
 
 for epoch in range(1, args.epochs + 1):
     train(epoch)
-    test(epoch)
+test(epoch)
+fresult.close()
