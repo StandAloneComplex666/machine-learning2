22efa00b2a558ef162dd3fb3a143bf807c162bed
StandAloneComplex666 <yc2739@nyu.edu>
2017-04-26 16:31:21 -0400

    Update pytorch_nn.py

--- pytorch_nn.py
+++ pytorch_nn.py
@@ -62,12 +62,11 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1_drop(self.conv1(x)), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drp[(self.conv2(x)), 2))
-                                               
+        x = F.relu(F.max_pool2d(self.conv2_drp[(self.conv2(x)), 2))                                               
         x = F.relu(self.conv3(x))
         x = x.view(-1, 18000)
         x = F.relu(self.fc1(x))
-        #x = F.dropout(x, training=self.training)
+        x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x)
 
